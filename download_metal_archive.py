8aabaf9e8f8158f6c6db66e1e319aa7bc3cb0e66
Ramiz Zeynalov <ramiz.zeynalov@gmail.com>
2024-12-29 01:23:41 +0100

    fix: Fix user agent issue, add gitignore, minor fixes

--- download_metal_archive.py
+++ download_metal_archive.py
@@ -1,3 +1,5 @@
+import time
+
 import requests
 import json 
 import pandas as pd
@@ -21,13 +23,26 @@ def download_url(letter, display_start):
     Besides that, the page only can handle 500 bands at once, so we have to change display_start until we 
     aren't getting anything. We will test this by checking the length of the response.
     """
+    print(letter, display_start)
     letter = letter.upper()
     display_start = display_start * 500
-    url = f"https://www.metal-archives.com/browse/ajax-letter/l/{letter}/json/1?sEcho=3&iColumns=4&sColumns=&iDisplayStart={display_start}&iDisplayLength=500&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&iSortCol_0=0&sSortDir_0=asc&iSortingCols=1&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=false&_=1519758159154"
-    data_json = json.loads(requests.get(url).text)['aaData'] 
+    epoch_time = get_current_epoch_time()
+    url = f"https://www.metal-archives.com/browse/ajax-letter/l/{letter}/json/1?sEcho=2&iColumns=4&sColumns=&iDisplayStart={display_start}&iDisplayLength=500&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&iSortCol_0=0&sSortDir_0=asc&iSortingCols=1&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=false&_={epoch_time}"
+    print(url)
+
+    text = requests.get(url, headers={'User-Agent': ''}).text # By default user-agent is python-requests, for which query parameters are ignored
+    json_data = json.loads(text)
+    data_json = json_data['aaData']
     if len(data_json) == 0:
         return None
+    print(data_json[0][0])
     return list(map(parse_data, data_json))
+
+
+def get_current_epoch_time():
+    epoch_time = round(time.time() * 1000)
+    return epoch_time
+
 
 def download_letter(letter):
     """
@@ -35,20 +50,19 @@ def download_letter(letter):
     """
     letter_data = []
     for i in range(1000):
-        print(i)
-        prov_data = download_url('A', i)
-        if prov_data is None:
-            pd.DataFrame(letter_data).to_csv(letter + '.csv', index=None)
-            return None
-        else:
-            letter_data = letter_data + prov_data
+        try:
+            prov_data = download_url(letter, i)
+            if prov_data is None:
+                pd.DataFrame(letter_data).to_csv(f"{letter}.csv", index=None)
+                return None
+            else:
+                letter_data = letter_data + prov_data
+        except Exception as e:
+            print(e)
 
+for letter in 'ABCDEFGHIJKLMNOPQRSTUVXWYZ~':
+    print(f"Downloading for letter {letter}")
+    download_letter(letter)
 
-
-#for i in 'ABCDEFGHIJKLMNOPQRSTUVXWYZ~':
-for i in 'FGHIJKLMNOPQRSTUVXWYZ~':
-    print(i)
-    download_letter(i)
-
-
+# Special case for '#'
 download_letter('NBR')
